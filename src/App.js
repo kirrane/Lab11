0915b180f56c7a7917891e2de2e557dea742573d
kirrane <g00349777@gmit.ie>
2020-10-16 12:23:26 +0100

    test

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-
-function App() {
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/content';
+//import bootstrap 
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Navbar, Nav} from 'react-bootstrap';
+import {BrowserRouter as Router, Switch, Route} from 'react-router-dom' ;
+//create a class instead of  function so you can have multiple classes
+class App extends Component {
+  //render so class will run
+  render(){
   return (
+    <Router>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<Navbar bg="primary" variant="dark">
+    <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+    <Nav className="mr-auto">
+      <Nav.Link href="/">Home</Nav.Link>
+      <Nav.Link href="/read">Read</Nav.Link>
+      <Nav.Link href="/create">Create</Nav.Link>
+    </Nav>
+  </Navbar>
+
+  <br />
+  <Switch>
+    <Route path='/' component={Content} exact/>
+    <Route path='/create' component={Header} exact/>
+    <Route path='/read' component={Footer} exact/>
+  </Switch>
     </div>
+    </Router>
   );
 }
+}// end render
 
 export default App;
---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+//class for header
+export class Footer extends React.Component{
+
+        render(){
+            return(
+                <div>
+                    <h1>This is the Footer Component</h1>
+                </div>
+
+            );
+        }
+}
